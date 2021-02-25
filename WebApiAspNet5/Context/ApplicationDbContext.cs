351fb8fb462f608255a371ae769f9c48796f8bbc
Leanderp <leanderperez15@gmail.com>
2021-02-25 18:33:12 -0400

    Agregar archivos de proyecto.

---
+++ WebApiAspNet5/Context/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAspNet5.Entities;
+
+namespace WebApiAspNet5.Context
+{
+    public class ApplicationDbContext : DbContext 
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+
+        }
+
+        public DbSet<Autor> Autores { get; set; }
+    }
+
+}
---
+++ WebApiAspNet5/Controllers/AutoresController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiAspNet5.Context;
+using WebApiAspNet5.Entities;
+
+namespace WebApiAspNet5.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AutoresController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public AutoresController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Autor>> Get()
+        {
+            return _context.Autores.ToList();
+        }
+    }
+}
