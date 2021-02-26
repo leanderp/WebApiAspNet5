21a6aef17833068863e52cb03ec5b2467b423f8d
Leanderp <leanderperez15@gmail.com>
2021-02-26 11:08:44 -0400

    cambios en errores y refactorizacion

--- WebApiAspNet5/Controllers/AutoresController.cs
+++ WebApiAspNet5/Controllers/AutoresController.cs
@@ -20,32 +20,37 @@ namespace WebApiAspNet5.Controllers
             _context = context;
         }
 
+        // GET: api/Autores
         [HttpGet]
         public ActionResult<IEnumerable<Autor>> Get()
         {
             return _context.Autores.ToList();
         }
 
+        // GET: api/Autores/1
         [HttpGet("{id}", Name ="ObtenerAutor")]
         public ActionResult<Autor> Get(int id)
         {
-            var autor = _context.Autores.FirstOrDefault(x => x.Id == id);
-            if(autor != null)
+            var autor = _context.Autores.Find(id);
+            if(autor == null)
             {
-                return autor;
+                return NotFound();
             }
 
-            return NotFound();
+            return autor;
         }
 
+        // POST: api/Autores
         [HttpPost]
         public ActionResult Post([FromBody]Autor autor)
         {
             _context.Autores.Add(autor);
             _context.SaveChanges();
+            
             return new CreatedAtRouteResult("ObtenerAutor", new { id = autor.Id}, autor);
         }
 
+        // PUT: api/Autores/1
         [HttpPut("{id}")]
         public ActionResult Put(int id, [FromBody] Autor value)
         {
@@ -55,14 +60,24 @@ namespace WebApiAspNet5.Controllers
             }
 
             _context.Entry(value).State = EntityState.Modified;
-            _context.SaveChanges();
+
+            try
+            {
+                _context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException) when (!AutorExiste(id))
+            {
+                return NotFound();
+            }
+
             return Ok();
         }
 
+        // DELETE: api/Autores/1
         [HttpDelete("{id}")]
         public ActionResult<Autor> Delete(int id)
         {
-            var autor = _context.Autores.FirstOrDefault(x => x.Id == id);
+            var autor = _context.Autores.Find(id);
 
             if (autor == null)
             {
@@ -73,5 +88,9 @@ namespace WebApiAspNet5.Controllers
             _context.SaveChanges();
             return autor;
         }
+
+        private bool AutorExiste(long id) =>
+            _context.Autores.Any(a => a.Id == id);
+
     }
 }
