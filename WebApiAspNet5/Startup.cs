ecbcb6ef8595bb5c31fb287bf55dd9d2e341fb0a
Leanderp <leanderperez15@gmail.com>
2021-03-02 17:06:51 -0400

    configurar ambiente para peticiones de frontend

--- WebApiAspNet5/Startup.cs
+++ WebApiAspNet5/Startup.cs
@@ -28,6 +28,8 @@ namespace WebApiAspNet5
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+
+            services.AddCors();
             services.AddControllers().AddNewtonsoftJson(options =>
                 options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
             );
@@ -44,6 +46,13 @@ namespace WebApiAspNet5
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseCors(options =>
+            {
+                options.WithOrigins("http://localhost:3000");
+                options.AllowAnyMethod();
+                options.AllowAnyHeader();
+            });
+            
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
