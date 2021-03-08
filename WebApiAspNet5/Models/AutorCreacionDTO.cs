e693cb3cc3621b55675f6c47ff0954e444753eb1
Leanderp <leanderperez15@gmail.com>
2021-03-08 18:47:15 -0400

    correcion modelo

--- WebApiAspNet5/Models/AutorCreacionDTO.cs
+++ WebApiAspNet5/Models/AutorCreacionDTO.cs
@@ -3,12 +3,15 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApiAspNet5.Helpers;
 
 namespace WebApiAspNet5.Models
 {
     public class AutorCreacionDTO
     {
         [Required]
+        [StringLength(50, ErrorMessage = "El Nombre del autor debe tener {1} carateres o menos")]
+        [PrimeraLetraMayuscula]
         public string Nombre { get; set; }
         public DateTime FechaNacimiento { get; set; }
     }
